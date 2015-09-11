9d787fb123c16f6029557e22111e2150ee9d8a16
gusdnide <gusdnide99@hotmail.com>
2015-09-11 19:05:36 -0300

    1.0

---
+++ Proxy Tools/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Proxy_Tools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.listBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 386);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Listas";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(486, 373);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "by:gusdnide";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(9, 45);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(199, 329);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lista de Proxys:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(291, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Proxys Funcionando:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(248, 45);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(199, 329);
+            this.listBox2.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(470, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(145, 154);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Botoes";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pegar Proxys";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(6, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Verificar Proxys";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Location = new System.Drawing.Point(469, 173);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(145, 170);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Opçoes";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(5, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 38);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Copiar Proxys Funcionando";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 28);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 38);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Salvar Proxys Funcionando";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(7, 116);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(122, 38);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Salvar Tudo";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(619, 403);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Proxy Tools";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Proxy Tools/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+namespace Proxy_Tools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void Salvar(ListBox lst)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = "";
+            sfd.Filter = "Arquivos de Texto (*.txt)|*.txt";
+            sfd.Title = "Procure...";
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string[] g = new string[lst.Items.Count];
+                for (int i = 0; i < lst.Items.Count; i++)
+                {
+                    g[i] = lst.Items[i].ToString();
+                }
+                File.WriteAllLines(sfd.FileName, g);
+            }
+        }
+        void Copiar(ListBox lst)
+        {
+            string strPraCopiar = "";
+            foreach (string str in lst.Items)
+            {
+                strPraCopiar = strPraCopiar + str + Environment.NewLine;
+            }
+            if (strPraCopiar != string.Empty) Clipboard.SetText(strPraCopiar); 
+        }
+        void PegarLista()
+        {
+            ListBox lst = listBox1;
+            Thread t1 = new Thread(() => PegarProxys(lst, "01"));
+            Thread t2 = new Thread(() => PegarProxys(lst, "02"));
+            Thread t3 = new Thread(() => PegarProxys(lst, "03"));
+            Thread t4 = new Thread(() => PegarProxys(lst, "04"));
+            Thread t5 = new Thread(() => PegarProxys(lst, "05"));
+            t1.IsBackground = true;
+            t2.IsBackground = true;
+            t3.IsBackground = true;
+            t4.IsBackground = true;
+            t5.IsBackground = true;
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            t5.Start();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            PegarLista();
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+        void PegarProxys(ListBox list, string Page = "01")
+        {
+            string site = "http://www.samair.ru/proxy/proxy-" + Page + ".htm";
+            WebClient wc = new WebClient();
+            string gg = wc.DownloadString(site);
+            Regex ip = new Regex(@"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?):\d+");
+            MatchCollection result = ip.Matches(gg);
+            int g = result.Count;
+            for (int t = 0; t < g; t++)
+            {
+                this.Invoke((MethodInvoker)(() => list.Items.Add(result[t].ToString())));
+            }
+            
+        }
+        void ThreadVerificar()
+        {
+            Thread t = new Thread(() => Verificar(listBox1));
+            t.IsBackground = true;
+            t.Start();
+        }
+        void Verificar(ListBox lst)
+        {
+            ProxyTools pTools = new ProxyTools();
+     
+            foreach (string str in lst.Items)
+            {
+                ProxyTools.Proxy Proxy = new  ProxyTools.Proxy();
+                Proxy = Proxy.PegarProxy(str);
+                if (pTools.Verificar(Proxy.IP, Proxy.Port))
+                {
+                    this.Invoke((MethodInvoker)(() => listBox2.Items.Add(str)));
+                }
+                this.Invoke((MethodInvoker)(() => this.Text = "Proxy Tools Scaneando: " + str));
+            }
+            MessageBox.Show("Completo!");
+            this.Invoke((MethodInvoker)(() => button1.Enabled = true));
+            this.Invoke((MethodInvoker)(() => button2.Enabled = false));
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Salvar(listBox2);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Copiar(listBox2);
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Salvar(listBox1);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            listBox2.Items.Clear();
+            ThreadVerificar();
+        }
+    }
+    public class ProxyTools
+    {
+        public struct Proxy
+        {
+            public string IP;
+            public string Port;
+            public string IPPort() { return IP + ":" + Port; }
+            public Proxy PegarProxy(string IPPort)
+            {
+                Proxy px = new Proxy();
+                string[] Resultados = Separar(":", IPPort);
+                if (Resultados.Count() > 0)
+                {
+                    px.IP = Resultados[0];
+                    px.Port = Resultados[1];
+                }
+                return px;
+            }
+        }
+        struct APIProxy
+        {
+          public  enum _Tipo
+            {
+                Transparent,
+                Anonymous,
+                HighAnonymous
+            };
+            public double response_time;
+            public int speed;
+            public string Localizacao;
+            public _Tipo Tipo;
+            public _Tipo Parse(int ID)
+            {
+                switch (ID)
+                {
+                    case 0: return _Tipo.Transparent;
+                    case 1: return _Tipo.Anonymous;
+                    case 2: return _Tipo.HighAnonymous;
+                }
+                return _Tipo.Transparent;
+            }
+        }      
+        string PegarAPI(string IP, string Port)
+        {
+          
+                Stream dataStream;
+                WebRequest request = WebRequest.Create("http://api.proxyipchecker.com/pchk.php");
+                request.Method = "POST";
+
+                string postData = "ip=" + IP + "&port=" + Port;
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = byteArray.Length;
+                dataStream = request.GetRequestStream();
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                WebResponse response = request.GetResponse();
+                dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+                return responseFromServer;
+          
+        }
+        APIProxy PegarResultado(string str)
+        {
+            APIProxy APIRetorno = new APIProxy();
+            string[] Resultados = Separar(";", str);
+            APIRetorno.response_time = double.Parse(Resultados[0]);
+            APIRetorno.speed = int.Parse(Resultados[1]);
+            APIRetorno.Localizacao = Resultados[2];
+            APIRetorno.Tipo = APIRetorno.Parse(int.Parse(Resultados[3]));
+            return APIRetorno;
+        }
+        public static string[] Separar(string separator, string source)
+        {
+            return source.Split(new string[] { separator }, StringSplitOptions.None);
+        }
+        public  bool Verificar(string IP, string Port)
+        {
+            string Resposta = PegarAPI(IP, Port);
+            
+                APIProxy AP = new APIProxy();
+                AP = PegarResultado(Resposta);
+                if (AP.response_time > 0.5)
+                {
+                    return true;
+                }
+        
+            return false;
+        }
+    }
+}
